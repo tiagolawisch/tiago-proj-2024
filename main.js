e83ec13341f9dcb20f53a104d1433efe14c5d0c9
tiagolawisch <135342534+tiagolawisch@users.noreply.github.com>
2024-02-29 08:14:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const botoes = document.querySelectorAll(<<.botao>>);
+console.log(botoes)
+for (let i=0; i < botoes.length; i++){console.log(i);}
+for (let i=0;i <botoes.length;i++){
+    botoes[i].onclick = Function() {
+    botoes[i].classList.add(<<ativo>>);}}
